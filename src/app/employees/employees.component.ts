4e6c105fd3bf96ef5e29e6a33a69ffb7649a3fb2
S.Haturusinghe <haturusingh3@gmail.com>
2022-10-14 17:32:36 +0530

    employee component commit 1

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import employees from './data/employees.json';
+
 @Component({
   selector: 'em-employees',
   templateUrl: './employees.component.html',
-  styleUrls: ['./employees.component.scss']
+  styleUrls: ['./employees.component.scss'],
 })
 export class EmployeesComponent implements OnInit {
+  title: string = 'Employee Management System';
+  employees: any[] = employees;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
